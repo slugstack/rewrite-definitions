7527f16179795f5ec610cd99df548c232d474b67
Aaron Gershman <aegershman@gmail.com>
2021-02-25 18:48:16 -0600

    ghp

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,8 +35,8 @@ publishing {
         //     name = "GitHubPackages"
         //     url = uri("https://maven.pkg.github.com/OWNER/REPOSITORY")
         //     credentials {
-        //         username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
-        //         password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
+        //         username = project.findProperty("gpr.user") as String? ?: System.getenv("GITHUB_ACTOR")
+        //         password = project.findProperty("gpr.key") as String? ?: System.getenv("GITHUB_TOKEN")
         //     }
         // }
 
