91610561b8bc559b190add08d480e86719ba4488
Aaron Gershman <aegershman@gmail.com>
2021-02-25 20:04:42 -0600

    IRRITATED

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
     java
     `maven-publish`
     signing // todo
+    id("io.github.gradle-nexus.publish-plugin") version "1.0.0" // https://github.com/gradle-nexus/publish-plugin
     id("nebula.release") version "15.2.0"
 }
 
@@ -45,41 +46,64 @@ java {
     withSourcesJar()
 }
 
-publishing {
-    repositories {
-        // todo just example
-        // maven {
-        //     name = "GitHubPackages"
-        //     url = uri("https://maven.pkg.github.com/OWNER/REPOSITORY")
-        //     credentials {
-        //         username = project.findProperty("gpr.user") as String? ?: System.getenv("GITHUB_ACTOR")
-        //         password = project.findProperty("gpr.key") as String? ?: System.getenv("GITHUB_TOKEN")
-        //     }
-        // }
 
-        maven {
-            name = "OSSRH"
-            val releasesRepoUrl = "https://oss.sonatype.org/service/local/staging/deploy/maven2/"
-            val snapshotsRepoUrl = "https://oss.sonatype.org/content/repositories/snapshots/"
-            url = uri(if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl)
-            credentials {
-                username = project.findProperty("ossrhUsername") as String? ?: System.getenv("OSSRH_USERNAME") // alt, mavenCentralRepositoryUsername?
-                password = project.findProperty("ossrhPassword") as String? ?: System.getenv("OSSRH_PASSWORD") // alt, mavenCentralRepositoryPassword?
-            }
+// todo trying out plugin
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
         }
     }
-
-    publications.create<MavenPublication>("maven") {
-        from(components["java"])
-    }
-
-    // publications {
-    //     register("gpr") {
-    //         from(components["java"])
-    //     }
-    // }
-
 }
+nexusPublishing {
+    repositories {
+        create("mavenCentral") {
+            nexusUrl.set(uri("https://oss.sonatype.org/service/local/staging/deploy/maven2/"))
+            snapshotRepositoryUrl.set(uri("https://oss.sonatype.org/content/repositories/snapshots/"))
+            username.set("ossrhUsername") // defaults to project.properties["myNexusUsername"]
+            password.set("ossrhPassword") // defaults to project.properties["myNexusPassword"]
+        }
+    }
+}
+
+// publishing {
+//     repositories {
+//         // todo just example
+//         // maven {
+//         //     name = "GitHubPackages"
+//         //     url = uri("https://maven.pkg.github.com/OWNER/REPOSITORY")
+//         //     credentials {
+//         //         username = project.findProperty("gpr.user") as String? ?: System.getenv("GITHUB_ACTOR")
+//         //         password = project.findProperty("gpr.key") as String? ?: System.getenv("GITHUB_TOKEN")
+//         //     }
+//         // }
+
+//         maven {
+//             name = "OSSRH"
+//             val releasesRepoUrl = "https://oss.sonatype.org/service/local/staging/deploy/maven2/"
+//             val snapshotsRepoUrl = "https://oss.sonatype.org/content/repositories/snapshots/"
+//             url = uri(if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl)
+//             credentials {
+//                 username = project.findProperty("ossrhUsername") as String? ?: System.getenv("OSSRH_USERNAME") // alt, mavenCentralRepositoryUsername?
+//                 password = project.findProperty("ossrhPassword") as String? ?: System.getenv("OSSRH_PASSWORD") // alt, mavenCentralRepositoryPassword?
+//             }
+//         }
+//     }
+
+//     publications.create<MavenPublication>("maven") {
+//         from(components["java"])
+//     }
+
+//     // publications {
+//     //     register("gpr") {
+//     //         from(components["java"])
+//     //     }
+//     // }
+
+// }
+
+
+
 
 signing {
     // sign(publishing.publications["mavenJava"])
