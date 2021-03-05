030b871c412c0d90347b23c2344667e1209b32ef
Aaron Gershman <5619476+aegershman@users.noreply.github.com>
2021-03-05 14:09:38 -0600

    Similarities (#17)
    * sync similiarities
    
    * formatting

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,17 @@
+import com.github.jk1.license.LicenseReportExtension
+import nebula.plugin.contacts.Contact
+import nebula.plugin.contacts.ContactsExtension
+import nebula.plugin.info.InfoBrokerPlugin
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import java.util.*
+
+//java.sourceCompatibility = JavaVersion.VERSION_11
+//// java {
+////     withJavadocJar()
+////     withSourcesJar()
+//// }
 //
-// heading buffer
-//
+
 
 buildscript {
     repositories {
@@ -13,29 +24,66 @@ plugins {
     `maven-publish`
     signing
 
-    id("nebula.maven-resolved-dependencies") version "17.3.2" // testing
+    id("nebula.maven-resolved-dependencies") version "17.3.2"
     id("nebula.release") version "15.3.1"
-    id("io.github.gradle-nexus.publish-plugin") version "1.0.0" // https://github.com/gradle-nexus/publish-plugin
+    id("io.github.gradle-nexus.publish-plugin") version "1.0.0"
+
+    id("com.github.hierynomus.license") version "0.15.0" apply false
+    id("org.jetbrains.kotlin.jvm") version "1.4.21"
+    id("com.github.jk1.dependency-license-report") version "1.16"
 
     id("nebula.maven-publish") version "17.3.2"
     id("nebula.contacts") version "5.1.0"
     id("nebula.info") version "9.3.0"
 
-    id("nebula.javadoc-jar") version "17.3.2" // testing
-    id("nebula.source-jar") version "17.3.2" // testing
-    id("nebula.maven-apache-license") version "17.3.2" // https://plugins.gradle.org/plugin/nebula.maven-apache-license
+    id("nebula.javadoc-jar") version "17.3.2"
+    id("nebula.source-jar") version "17.3.2"
+    id("nebula.maven-apache-license") version "17.3.2"
 }
+
+group = "io.slugstack.oss"
+description = "slugstack-publishing-test examples"
+
 
 repositories {
     mavenLocal()
     mavenCentral()
-    // maven {
-    //     url = uri("https://jcenter.bintray.com/")
-    // }
+}
 
-    // maven {
-    //     url = uri("https://repo.maven.apache.org/maven2/")
-    // }
+
+nexusPublishing {
+    repositories {
+        sonatype {
+            username.set(project.findProperty("ossrhUsername") as String? ?: System.getenv("OSSRH_USERNAME"))
+            password.set(project.findProperty("ossrhToken") as String? ?: System.getenv("OSSRH_TOKEN"))
+        }
+    }
+}
+
+signing {
+    setRequired({
+        // skip signing snapshots by default, unless you explicitly opt-in with -PforceSigning
+        !project.version.toString().endsWith("SNAPSHOT") || project.hasProperty("forceSigning")
+    })
+    val signingKey = project.findProperty("signingKey") as String? ?: System.getenv("SIGNING_KEY")
+    val signingPassword = project.findProperty("signingPassword") as String? ?: System.getenv("SIGNING_PASSWORD")
+    useInMemoryPgpKeys(signingKey, signingPassword)
+    // useGpgCmd() // todo for local testing
+    sign(publishing.publications["nebula"])
+}
+
+configurations.all {
+    resolutionStrategy {
+        cacheChangingModulesFor(0, TimeUnit.SECONDS)
+        cacheDynamicVersionsFor(0, TimeUnit.SECONDS)
+    }
+
+    // We use kotlin exclusively for tests
+    // The kotlin plugin adds kotlin-stdlib dependencies to the main sourceSet, even if it doesn't use any kotlin
+    // To avoid shipping dependencies we don't actually need, exclude them from the main sourceSet classpath but add them _back_ in for the test source sets
+    if (name == "compileClasspath" || name == "runtimeClasspath") {
+        exclude(group = "org.jetbrains.kotlin")
+    }
 }
 
 dependencies {
@@ -86,33 +134,136 @@ dependencies {
     // END TESTING BLOCK
 }
 
-group = "io.slugstack.oss"
-description = "slugstack-publishing-test examples"
 
-configure<nebula.plugin.contacts.ContactsExtension> {
-    val c = nebula.plugin.contacts.Contact("aegershman@gmail.com")
+
+tasks.named<Test>("test") {
+    useJUnitPlatform()
+    jvmArgs = listOf("-XX:+UnlockDiagnosticVMOptions", "-XX:+ShowHiddenFrames")
+}
+
+//tasks.withType<JavaCompile> {
+//    options.encoding = "UTF-8"
+//}
+tasks.named<JavaCompile>("compileJava") {
+    sourceCompatibility = JavaVersion.VERSION_1_8.toString()
+    targetCompatibility = JavaVersion.VERSION_1_8.toString()
+
+    options.isFork = true
+    options.forkOptions.executable = "javac"
+    options.compilerArgs.addAll(listOf("--release", "8"))
+    options.encoding = "UTF-8"
+    options.compilerArgs.add("-parameters")
+}
+
+tasks.withType(KotlinCompile::class.java).configureEach {
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+
+    doFirst {
+        destinationDir.mkdirs()
+    }
+}
+
+configure<ContactsExtension> {
+    val c = Contact("aegershman@gmail.com")
     c.moniker("Aaron Gershman")
     c.github("aegershman")
     people["aegershman@gmail.com"] = c
 }
 
-java.sourceCompatibility = JavaVersion.VERSION_11
 
-tasks.withType<JavaCompile> {
-    options.encoding = "UTF-8"
+// configure<LicenseExtension> {
+//     ext.set("year", Calendar.getInstance().get(Calendar.YEAR))
+//     skipExistingHeaders = true
+//     header = project.rootProject.file("gradle/licenseHeader.txt")
+//     mapping(mapOf("kt" to "SLASHSTAR_STYLE", "java" to "SLASHSTAR_STYLE"))
+//     // exclude JavaTemplate shims from license check
+//     exclude("src/main/resources/META-INF/rewrite/*.java")
+//     strictCheck = true
+// }
+
+configure<LicenseReportExtension> {
+    renderers = arrayOf(com.github.jk1.license.render.CsvReportRenderer())
 }
 
-configurations.all {
-    resolutionStrategy {
-        cacheChangingModulesFor(0, TimeUnit.SECONDS)
-        cacheDynamicVersionsFor(0, TimeUnit.SECONDS)
+configure<PublishingExtension> {
+    publications {
+        named("nebula", MavenPublication::class.java) {
+            suppressPomMetadataWarningsFor("runtimeElements")
+
+            pom.withXml {
+                (asElement().getElementsByTagName("dependencies").item(0) as org.w3c.dom.Element).let { dependencies ->
+                    dependencies.getElementsByTagName("dependency").let { dependencyList ->
+                        var i = 0
+                        var length = dependencyList.length
+                        while (i < length) {
+                            (dependencyList.item(i) as org.w3c.dom.Element).let { dependency ->
+                                if ((dependency.getElementsByTagName("scope")
+                                        .item(0) as org.w3c.dom.Element).textContent == "provided"
+                                    || (dependency.getElementsByTagName("groupId")
+                                        .item(0) as org.w3c.dom.Element).textContent == "org.jetbrains.kotlin"
+                                ) {
+                                    dependencies.removeChild(dependency)
+                                    i--
+                                    length--
+                                }
+                            }
+                            i++
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+tasks.withType<GenerateMavenPom> {
+    doLast {
+        // because pom.withXml adds blank lines
+        destination.writeText(
+            destination.readLines().filter { it.isNotBlank() }.joinToString("\n")
+        )
     }
 
-    // We use kotlin exclusively for tests
-    // The kotlin plugin adds kotlin-stdlib dependencies to the main sourceSet, even if it doesn't use any kotlin
-    // To avoid shipping dependencies we don't actually need, exclude them from the main sourceSet classpath but add them _back_ in for the test source sets
-    if (name == "compileClasspath" || name == "runtimeClasspath") {
-        exclude(group = "org.jetbrains.kotlin")
+    doFirst {
+        val runtimeClasspath = configurations.getByName("runtimeClasspath")
+
+        val gav = { dep: ResolvedDependency ->
+            "${dep.moduleGroup}:${dep.moduleName}:${dep.moduleVersion}"
+        }
+
+        val observedDependencies = TreeSet<ResolvedDependency> { d1, d2 ->
+            gav(d1).compareTo(gav(d2))
+        }
+
+        fun reduceDependenciesAtIndent(indent: Int):
+                    (List<String>, ResolvedDependency) -> List<String> =
+            { dependenciesAsList: List<String>, dep: ResolvedDependency ->
+                dependenciesAsList + listOf(" ".repeat(indent) + dep.module.id.toString()) + (
+                        if (observedDependencies.add(dep)) {
+                            dep.children
+                                .sortedBy(gav)
+                                .fold(emptyList(), reduceDependenciesAtIndent(indent + 2))
+                        } else {
+                            // this dependency subtree has already been printed, so skip it
+                            emptyList()
+                        }
+                        )
+            }
+
+        project.plugins.withType<InfoBrokerPlugin> {
+            add(
+                "Resolved-Dependencies", runtimeClasspath
+                    .resolvedConfiguration
+                    .lenientConfiguration
+                    .firstLevelModuleDependencies
+                    .sortedBy(gav)
+                    .fold(emptyList(), reduceDependenciesAtIndent(6))
+                    .joinToString("\n", "\n", "\n" + " ".repeat(4))
+            )
+        }
     }
 }
 
@@ -121,30 +272,4 @@ configure<nebula.plugin.release.git.base.ReleasePluginExtension> {
     // Inferred project: slugstack-publishing-test, version: 0.1.0-dev.1.uncommitted+ae68a9d
     // but WITH this config, ./gradlew build defaults to: Inferred project: slugstack-publishing-test, version: 0.1.0-SNAPSHOT
     defaultVersionStrategy = nebula.plugin.release.NetflixOssStrategies.SNAPSHOT(project)
-}
-
-// java {
-//     withJavadocJar()
-//     withSourcesJar()
-// }
-
-nexusPublishing {
-    repositories {
-        sonatype {
-            username.set(project.findProperty("ossrhUsername") as String? ?: System.getenv("OSSRH_USERNAME"))
-            password.set(project.findProperty("ossrhToken") as String? ?: System.getenv("OSSRH_TOKEN"))
-        }
-    }
-}
-
-signing {
-    setRequired({
-        // skip signing snapshots by default, unless you explicitly opt-in with -PforceSigning
-        !project.version.toString().endsWith("SNAPSHOT") || project.hasProperty("forceSigning")
-    })
-    val signingKey = project.findProperty("signingKey") as String? ?: System.getenv("SIGNING_KEY")
-    val signingPassword = project.findProperty("signingPassword") as String? ?: System.getenv("SIGNING_PASSWORD")
-    useInMemoryPgpKeys(signingKey, signingPassword)
-    // useGpgCmd() // todo for local testing
-    sign(publishing.publications["nebula"])
 }
